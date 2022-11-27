ec76aa6bf6964bbafaebe9e0e152cf7f7a0cdd0c
darka <luca@ahd-creative.com>
2022-11-27 11:59:10 +0100

    Italian Strings

--- XIVITAGuide/Localization/LocalizationStrings.cs
+++ XIVITAGuide/Localization/LocalizationStrings.cs
@@ -8,10 +8,10 @@ namespace XIVITAGuide.Localization
     /// </summary>
     internal sealed class TCommands
     {
-        internal static string GuideListHelp => Loc.Localize("Commands.DutyList.Help", "Toggles the guide list");
-        internal static string SettingsHelp => Loc.Localize("Commands.Settings.Help", "Toggles the settings menu");
-        internal static string EditorHelp => Loc.Localize("Commands.Editor.Help", "Toggles the guide editor");
-        internal static string InfoHelp => Loc.Localize("Commands.Info.Help", "Toggles the guide viewer window");
+        internal static string GuideListHelp => Loc.Localize("Commands.DutyList.Help", "Mostra la lista delle guide");
+        internal static string SettingsHelp => Loc.Localize("Commands.Settings.Help", "Mostra il menu delle impostazioni");
+        internal static string EditorHelp => Loc.Localize("Commands.Editor.Help", "Apri o Chiudi l'editor della guida");
+        internal static string InfoHelp => Loc.Localize("Commands.Info.Help", "Apri o Chiudi la finestra delle guide");
     }
 
     /// <summary>
@@ -30,24 +30,24 @@ namespace XIVITAGuide.Localization
     /// </summary>
     internal static class TGenerics
     {
-        internal static string Donate => Loc.Localize("Generics.Donate", "Donate");
-        internal static string OpenFile => Loc.Localize("Generics.OpenFile", "Open File");
-        internal static string SaveFile => Loc.Localize("Generics.SaveFile", "Save File");
-        internal static string Search => Loc.Localize("Generics.Search", "Search");
+        internal static string Donate => Loc.Localize("Generics.Donate", "Dona");
+        internal static string OpenFile => Loc.Localize("Generics.OpenFile", "Apri File");
+        internal static string SaveFile => Loc.Localize("Generics.SaveFile", "Salva File");
+        internal static string Search => Loc.Localize("Generics.Search", "Cerca");
         internal static string Guide => Loc.Localize("Generics.Guide", "Guide");
         internal static string InDuty => Loc.Localize("Generics.InDuty", "In Duty");
-        internal static string Mechanic => Loc.Localize("Generics.Mechanic", "Mechanic");
-        internal static string Mechanics => Loc.Localize("Generics.Mechanics", "Mechanics");
-        internal static string Strategy => Loc.Localize("Generics.Strategy", "Strategy");
-        internal static string Notes => Loc.Localize("Generics.Notes", "Notes");
-        internal static string Description => Loc.Localize("Generics.Description", "Description");
-        internal static string Level => Loc.Localize("Generics.Level", "Level");
-        internal static string Type => Loc.Localize("Generics.Type", "Type");
-        internal static string Enabled => Loc.Localize("Generics.Enabled", "Enabled");
-        internal static string Disabled => Loc.Localize("Generics.Disabled", "Disabled");
-        internal static string Unknown => Loc.Localize("Generics.Unknown", "Unknown");
-        internal static string Unspecified => Loc.Localize("Generics.Unspecified", "Unspecified");
-        internal static string None => Loc.Localize("Generics.None", "None");
+        internal static string Mechanic => Loc.Localize("Generics.Mechanic", "Meccanica");
+        internal static string Mechanics => Loc.Localize("Generics.Mechanics", "Meccaniche");
+        internal static string Strategy => Loc.Localize("Generics.Strategy", "Strategia");
+        internal static string Notes => Loc.Localize("Generics.Notes", "Note");
+        internal static string Description => Loc.Localize("Generics.Description", "Descrizione");
+        internal static string Level => Loc.Localize("Generics.Level", "Livello");
+        internal static string Type => Loc.Localize("Generics.Type", "Tipo");
+        internal static string Enabled => Loc.Localize("Generics.Enabled", "Abilitato");
+        internal static string Disabled => Loc.Localize("Generics.Disabled", "Disabilitato");
+        internal static string Unknown => Loc.Localize("Generics.Unknown", "Sconosciuto");
+        internal static string Unspecified => Loc.Localize("Generics.Unspecified", "Non specificato");
+        internal static string None => Loc.Localize("Generics.None", "Nessuno");
     }
 
     /// <summary>
@@ -55,8 +55,8 @@ namespace XIVITAGuide.Localization
     /// </summary>
     internal static class TGuide
     {
-        internal static string DutyUnnamed => Loc.Localize("Types.Duty.Name.None", "Unnamed Guide");
-        internal static string NoStrategy => Loc.Localize("Guide.NoStrategy", "No strategy available.");
+        internal static string DutyUnnamed => Loc.Localize("Types.Duty.Name.None", "Guida senza nome");
+        internal static string NoStrategy => Loc.Localize("Guide.NoStrategy", "Nessuna strategia disponibile.");
     }
 
     /// <summary>
@@ -64,19 +64,19 @@ namespace XIVITAGuide.Localization
     /// </summary>
     internal static class TEditor
     {
-        internal static string Problems => Loc.Localize("Editor.Problems", "Problems");
+        internal static string Problems => Loc.Localize("Editor.Problems", "Problemi");
         internal static string Preview => Loc.Localize("Editor.Preview", "Preview");
-        internal static string Clear => Loc.Localize("Editor.Clear", "Clear");
-        internal static string Format => Loc.Localize("Editor.Format", "Format");
+        internal static string Clear => Loc.Localize("Editor.Clear", "Pulisci");
+        internal static string Format => Loc.Localize("Editor.Format", "Formatta");
         internal static string Metadata => Loc.Localize("Editor.Metadata", "Metadata");
-        internal static string EnumList => Loc.Localize("Editor.EnumList", "Enum List");
-        internal static string CannotParseNoPreview => Loc.Localize("Editor.CannotParse", "Cannot parse file, unable to preview.");
-        internal static string ContributingGuide => Loc.Localize("Editor.ContributingGuide", "Contributing Guide");
-        internal static string ProblemUnsupported => Loc.Localize("Editor.Problem.Unsupported", "Guide contains invalid IDs and/or is unsupported by this plugin version.");
-        internal static string NoProblems => Loc.Localize("Editor.Problem.None", "No problems detected.");
-        internal static string FileTooLarge => Loc.Localize("Editor.FileTooBig", "Your file is too big to be loaded into the editor.");
-        internal static string FileSuccessfullyLoaded => Loc.Localize("Editor.FileLoaded", "Your file was successfully loaded.");
-        internal static string FileSuccessfullySaved => Loc.Localize("Editor.FileSaved", "Your file was successfully saved.");
+        internal static string EnumList => Loc.Localize("Editor.EnumList", "Lista Enum");
+        internal static string CannotParseNoPreview => Loc.Localize("Editor.CannotParse", "Non riesco a leggere il file, non posso visualizzare una anteprima.");
+        internal static string ContributingGuide => Loc.Localize("Editor.ContributingGuide", "Guida alla contribuzione");
+        internal static string ProblemUnsupported => Loc.Localize("Editor.Problem.Unsupported", "La guida contiente ID Invalidi o non supportati da questa versione.");
+        internal static string NoProblems => Loc.Localize("Editor.Problem.None", "Nessun problema rilevato.");
+        internal static string FileTooLarge => Loc.Localize("Editor.FileTooBig", "Il tuo file pesa troppo per essere caricato dentro questo editor.");
+        internal static string FileSuccessfullyLoaded => Loc.Localize("Editor.FileLoaded", "File caricato con successo.");
+        internal static string FileSuccessfullySaved => Loc.Localize("Editor.FileSaved", "File salvato con successo.");
     }
 
     /// <summary>
@@ -84,19 +84,19 @@ namespace XIVITAGuide.Localization
     /// </summary>
     internal static class TSettings
     {
-        internal static string Configuration => Loc.Localize("Settings.Configuration", "Configuration");
-        internal static string AutoOpenGuideForDuty => Loc.Localize("Settings.AutOpenGuideForDuty", "Auto Open Guide for Duty");
-        internal static string SettingsAutoOpenInDutyTooltip => Loc.Localize("Settings.AutoOpenDuty.Tooltip", "Automatically open/hide the guide for the given duty when you enter/leave it.");
-        internal static string ShortenGuideText => Loc.Localize("Settings.ShortedGuideText", "Shorten Guide Text");
-        internal static string ShortenGuideTextTooltip => Loc.Localize("Settings.ShortenGuideText.Tooltip", "Shorten text in all guides when available to help make them more accessible and save space.");
-        internal static string ShowDonateButton => Loc.Localize("Settings.ShowDonateButton", "Show Donate Button");
-        internal static string ShowDonateButtonTooltip => Loc.Localize("Settings.ShowDonateButton.Tooltip", "Show donate buttons in the plugin to help support the plugin and its development.");
-        internal static string HideLockedGuides => Loc.Localize("Settings.HideLockedGuides", "Hide Locked Guides");
-        internal static string HideLockedGuidesTooltip => Loc.Localize("Settings.HideLockedGuides.Tooltip", "Hide guides that you have not unlocked yet from the guide list and prevent opening them in the guide viewer.\nWARNING: Turning this off will show you spoilers for guides you have not unlocked yet.");
-        internal static string HiddenMechanics => Loc.Localize("Settings.HiddenMechanics", "Hidden Mechanics");
-        internal static string HiddenMechanicsTooltip => Loc.Localize("Settings.HiddenMechanics.Tooltip", "Hide mechanic types, preventing them from being shown in any guides.");
-        internal static string EnabledIntegrations => Loc.Localize("Settings.EnabledIntegrations", "Enabled Integrations");
-        internal static string EnabledIntegrationsTooltip => Loc.Localize("Settings.EnabledIntegrations.Tooltip", "Enable or disable integrations with other plugins. Please note that the plugin(s) must be installed for the integration to work.");
+        internal static string Configuration => Loc.Localize("Settings.Configuration", "Configurazione");
+        internal static string AutoOpenGuideForDuty => Loc.Localize("Settings.AutOpenGuideForDuty", "Apri guida quando in Duty");
+        internal static string SettingsAutoOpenInDutyTooltip => Loc.Localize("Settings.AutoOpenDuty.Tooltip", "Apre la guida associata appena si entra in duty e la chiude quando si esce.");
+        internal static string ShortenGuideText => Loc.Localize("Settings.ShortedGuideText", "Riassunto Guida");
+        internal static string ShortenGuideTextTooltip => Loc.Localize("Settings.ShortenGuideText.Tooltip", "Riassume il testo nelle guide quando possibile per renderle più accessibili e salvare spazio.");
+        internal static string ShowDonateButton => Loc.Localize("Settings.ShowDonateButton", "Mostra il pulsante di donazione");
+        internal static string ShowDonateButtonTooltip => Loc.Localize("Settings.ShowDonateButton.Tooltip", "Mostra il pulsante di donazione per aiutare lo sviluppo di questo plugin.");
+        internal static string HideLockedGuides => Loc.Localize("Settings.HideLockedGuides", "Nascondi guide per duty non ancora sbloccati");
+        internal static string HideLockedGuidesTooltip => Loc.Localize("Settings.HideLockedGuides.Tooltip", "Nascondee le guide per i duty non ancora sbloccati.\nATTENZIONE: impostandolo su OFF, vedrete tutte le guide. Anche quelle per i duty non ancora sbloccati.");
+        internal static string HiddenMechanics => Loc.Localize("Settings.HiddenMechanics", "Meccaniche nascoste");
+        internal static string HiddenMechanicsTooltip => Loc.Localize("Settings.HiddenMechanics.Tooltip", "Nasconde la tipologia delle meccaniche, nascondendole così dalle guide.");
+        internal static string EnabledIntegrations => Loc.Localize("Settings.EnabledIntegrations", "Integrazioni Abilitate");
+        internal static string EnabledIntegrationsTooltip => Loc.Localize("Settings.EnabledIntegrations.Tooltip", "Abilita o Disabilita le integrazioni con altri plugin. Ovviamente i plugin abilitati devono essere prima installati.");
     }
 
     /// <summary>
@@ -104,16 +104,16 @@ namespace XIVITAGuide.Localization
     /// </summary>
     internal static class TGuideViewer
     {
-        internal static string NoGuideInfoAvailable => Loc.Localize("GuideViewer.NoGuideInfoAvailable", "There is no information available for this guide right now.");
-        internal static string NoPhaseInfoAvailable => Loc.Localize("GuideViewer.NoPhaseInfoAvailable", "There is no information available for this phase right now.");
-        internal static string ReportIssueWithGuide => Loc.Localize("GuideViewer.ReportIssueWithGuide", "Report issue with guide");
-        internal static string UnlockWindowMovement => Loc.Localize("GuideViewer.UnlockWindowMovement", "Unlock window");
-        internal static string LockWindowMovement => Loc.Localize("GuideViewer.LockWindowMovement", "Lock window");
-        internal static string ToggleSettingsWindow => Loc.Localize("GuideViewer.ToggleSettingsWindow", "Toggle settings window");
-        internal static string GuideHeading(string guideName) => string.Format(Loc.Localize("GuideViewer.GuideHeading", "Guide for {0}"), guideName);
-        internal static string NoGuideSelected => Loc.Localize("GuideViewer.NoGuideSelected", "No guide selected, use /xivita-list to see all available guides.");
-        internal static string GuideNotUnlocked => Loc.Localize("GuideViewer.GuideInfoNotUnlocked", "You cannot view the guide for this duty as you have not unlocked it yet.");
-        internal static string GuideAvailableForDuty => Loc.Localize("GuideViewer.GuideAvailableForDuty", "A guide is available for this duty, use /xivita-info to view it.");
+        internal static string NoGuideInfoAvailable => Loc.Localize("GuideViewer.NoGuideInfoAvailable", "Non ci sono informazioni disponili per questa guida al momento.");
+        internal static string NoPhaseInfoAvailable => Loc.Localize("GuideViewer.NoPhaseInfoAvailable", "Non ci sono informazioni disponibili per questa fase al momento.");
+        internal static string ReportIssueWithGuide => Loc.Localize("GuideViewer.ReportIssueWithGuide", "Riporta un problema con questa guida");
+        internal static string UnlockWindowMovement => Loc.Localize("GuideViewer.UnlockWindowMovement", "Sblocca Finestra");
+        internal static string LockWindowMovement => Loc.Localize("GuideViewer.LockWindowMovement", "Blocca Finestra");
+        internal static string ToggleSettingsWindow => Loc.Localize("GuideViewer.ToggleSettingsWindow", "Mostra o nasconde il menù della finesta.");
+        internal static string GuideHeading(string guideName) => string.Format(Loc.Localize("GuideViewer.GuideHeading", "Guida per {0}"), guideName);
+        internal static string NoGuideSelected => Loc.Localize("GuideViewer.NoGuideSelected", "Non hai selezionato una guida, usa /xivita-list per vedere tutte le guide presenti.");
+        internal static string GuideNotUnlocked => Loc.Localize("GuideViewer.GuideInfoNotUnlocked", "Non puoi vedere la guida per questo Duty perchè non lo hai ancora sbloccato.");
+        internal static string GuideAvailableForDuty => Loc.Localize("GuideViewer.GuideAvailableForDuty", "Esiste una guida per questa duty. Use /xivita-info per vederla.");
         internal static string Lore => Loc.Localize("GuideViewer.Lore", "Lore");
     }
 
@@ -122,11 +122,11 @@ namespace XIVITAGuide.Localization
     /// </summary>
     internal static class TGuideListTable
     {
-        internal static string NoneFoundForType => Loc.Localize("GuideListTable.NoneFoundForType", "No guides found for this duty type.");
-        internal static string NoGuidesUnlocked => Loc.Localize("GuideListTable.NoGuidesUnlocked", "You have not unlocked any guides for this type.");
-        internal static string NoGuidesFoundForSearch => Loc.Localize("GuideListTable.NoGuidesFoundForSearch", "No guides found for your search.");
-        internal static string UnsupportedGuide(string guideName) => string.Format(Loc.Localize("GuideListTable.UnsupportedGuide", "The guide for {0} is for a different version of the plugin and cannot be loaded yet"), guideName);
-        internal static string NoGuideData(string guideName) => string.Format(Loc.Localize("GuideListTable.NoGuideData", "There is no data for {0} yet."), guideName);
-        internal static string NoGuidesFilesDetected => Loc.Localize("GuideListTable.NoGuidesFilesDetected", "No guide files were detected, please check your installation.");
+        internal static string NoneFoundForType => Loc.Localize("GuideListTable.NoneFoundForType", "Non ci sopno guide per questa tipologia di Duty.");
+        internal static string NoGuidesUnlocked => Loc.Localize("GuideListTable.NoGuidesUnlocked", "Non hai ancora sbloccato guide per questa tipologia di Duty.");
+        internal static string NoGuidesFoundForSearch => Loc.Localize("GuideListTable.NoGuidesFoundForSearch", "Non ho trovato guide dalla tua ricerca.");
+        internal static string UnsupportedGuide(string guideName) => string.Format(Loc.Localize("GuideListTable.UnsupportedGuide", "La guida per {0} è per una versione differente del plugin e non può essere caricata"), guideName);
+        internal static string NoGuideData(string guideName) => string.Format(Loc.Localize("GuideListTable.NoGuideData", "Non ho informazioni per {0} al momento."), guideName);
+        internal static string NoGuidesFilesDetected => Loc.Localize("GuideListTable.NoGuidesFilesDetected", "Non ho trovato guide installate. Controlla la tua installazione.");
     }
 }
